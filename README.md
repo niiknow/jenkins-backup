3a161205babf6d598ee7fe45647c9f5e3b246fee
noogen <tom@noogen.net>
2016-02-10 19:48:50 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# jenkins-backup
+Script that can run in Jenkins to backup Jenkins.  Say what?
+
+Execute shell: run.sh "/subfolder"
+
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Be pretty
+echo -e " "
+echo -e " .  ____  .    ______________________________"
+echo -e " |/	  \|   |                              |"
+echo -e "[| \e[1;31m♥    ♥\e[00m |]  | Jenkins Backup Script v.0.1 |"
+echo -e " |___==___|  /                © niiknow 2016 |"
+echo -e "              |______________________________|"
+echo -e " "
+
+# Basic variables, use environment variable
+backup_dest=$(sed 's/\/*$//' <<< "$1")
+JEKINS_HOME_DEFAULT="/var/jenkins_home"
+backup_target=${JENKINS_HOME:-$JEKINS_HOME_DEFAULT}
+backup_name="jenkins-$BUILD_NUMBER"
+
+# Timestamp (sortable AND readable)
+stamp=`date +"%Y%m%dT%H%M%S"`
+workfolder=`pwd`
+backup_filename="$stamp-$backup_name.tar.gz"
+
+# change to backup dir - creating archives with absolute paths can be dangerous
+cd "$backup_target"
+tar --exclude-from "$workfolder/backup_exclusions" -czf "$workfolder/$backup_filename" .
